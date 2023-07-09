4b04b5155a3545191d23f90ac90b0122abd6cda9
arperevalov <lovetraindriver@gmail.com>
2023-07-09 22:47:21 +0700

    fix: get_current_records text format

--- src/main.rs
+++ src/main.rs
@@ -74,8 +74,13 @@ fn get_current_records () {
     let base = log_core::get_json();
     let logs: Vec<Log> = base.app_current_logs;
     
+    if logs.len() == 0 {
+        return println!("There are no records yet! Try run \"changelog new <STRING>\" to make your first record.")
+    }
+    
+    println!("Changes of this version:");
     for item in logs {
-        println!("{}", item.text);
+        println!("— {}", item.text);
     }
 }
 
